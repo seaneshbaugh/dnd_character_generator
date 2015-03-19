a78211c5c3325613ea67308f9f26bbd7a873c70c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-03-19 18:24:32 -0500

    Fixed config so React gets loaded properly. Set HAML attr_wrapper to double quotes.

--- config.rb
+++ config.rb
@@ -70,3 +70,11 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+set :haml, { attr_wrapper: '"' }
+
+activate :react
+
+after_configuration do
+  sprockets.append_path File.dirname(::React::Source.bundled_path_for('react.js'))
+end

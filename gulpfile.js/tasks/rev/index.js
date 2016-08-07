c915d96d45d013901dd4789c7510c560a3924b37
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-07 01:37:22 -0500

    Linting.

--- gulpfile.js/tasks/rev/index.js
+++ gulpfile.js/tasks/rev/index.js
@@ -1,23 +1,28 @@
-var config = require('../../config')
-if(!config.tasks.production.rev) return
+"use strict";
 
-var gulp         = require('gulp')
-var gutil        = require('gulp-util')
-var gulpSequence = require('gulp-sequence')
+const config = require("../../config");
 
-// If you are familiar with Rails, this task the equivalent of `rake assets:precompile`
-var revTask = function(cb) {
-  gulpSequence(
-    // 1) Add md5 hashes to assets referenced by CSS and JS files
-    'rev-assets',
-    // 2) Update asset references (images, fonts, etc) with reved filenames in compiled css + js
-    'rev-update-references',
-    // 3) Rev and compress CSS and JS files (this is done after assets, so that if a referenced asset hash changes, the parent hash will change as well
-    'rev-css',
-    // 4) Update asset references in HTML
-    'update-html',
-  cb)
+if (!config.tasks.production.rev) {
+  return;
 }
 
-gulp.task('rev', revTask)
-module.exports = revTask
+const gulp = require("gulp");
+const gulpSequence = require("gulp-sequence");
+
+// If you are familiar with Rails, this task the equivalent of `rake assets:precompile`
+const revTask = function(callback) {
+  gulpSequence(
+    // 1) Add md5 hashes to assets referenced by CSS and JS files
+    "rev-assets",
+    // 2) Update asset references (images, fonts, etc) with reved filenames in compiled css + js
+    "rev-update-references",
+    // 3) Rev and compress CSS and JS files (this is done after assets, so that if a referenced asset hash changes, the parent hash will change as well
+    "rev-css",
+    // 4) Update asset references in HTML
+    "update-html",
+    callback);
+};
+
+gulp.task("rev", revTask);
+
+module.exports = revTask;

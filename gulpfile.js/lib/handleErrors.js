852cbdc2dc75206e45e9d7dd19aa08d1c206c2be
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-06 20:52:41 -0500

    Prefer rest params over arguments.

--- gulpfile.js/lib/handleErrors.js
+++ gulpfile.js/lib/handleErrors.js
@@ -1,11 +1,13 @@
+/* eslint prefer-reflect: "off" */
+
 "use strict";
 
 const notify = require("gulp-notify");
 
-module.exports = function(errorObject, _callback) {
+module.exports = function(errorObject, _callback, ...args) {
   notify.onError(errorObject.toString().split(": ").
     join(":\n")).
-    apply(this, arguments);
+    apply(this, args);
 
   if (typeof this.emit === "function") {
     this.emit("end");
